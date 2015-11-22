c87c20cb9fedf0c6e3478ee19e963e30594e5d8c
William Tamplin <billytamplin@gmail.com>
2015-11-21 19:23:59 -0800

    Fixed typo for Subscribe

--- newsletter-side.php
+++ newsletter-side.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="wrapper-bg newsletter">
         <div class="inner">
-            <h3>Subcribe to the Newsletter</h3>  
+            <h3>Subscribe to the Newsletter</h3>  
             <h4>Get new blog and store updates delivered to your inbox.</h4>
             <form action="//semisweetdesigns.us12.list-manage.com/subscribe/post?u=fe76ad92bdd35d5808d870eb6&amp;id=d48439dd50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
                 <div class="form-group">
