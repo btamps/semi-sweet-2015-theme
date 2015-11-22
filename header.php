c2819c4da27bb0fdff04fdc720198c78e9f316f2
William Tamplin <billytamplin@gmail.com>
2015-11-21 20:45:36 -0800

    added new typekit code

--- header.php
+++ header.php
@@ -11,6 +11,9 @@
     echo $thumb[0]; // thumbnail url
 }
    ?>" />
+
+    <script src="//use.typekit.net/ewa7xsh.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
     <title>
     <?php
       wp_title( '–', true, 'right' );
@@ -30,7 +33,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script type="text/javascript" src="//use.typekit.net/ewa7xsh.js"></script>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
