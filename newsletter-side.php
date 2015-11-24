e2d768e539f5393a121dd58bc8c2d4a160f0e418
William Tamplin <billytamplin@gmail.com>
2015-11-23 21:02:50 -0800

    Added same label and input from bottom newsletter

--- newsletter-side.php
+++ newsletter-side.php
@@ -5,8 +5,8 @@
             <h4>Get new blog and store updates delivered to your inbox.</h4>
             <form action="//semisweetdesigns.us12.list-manage.com/subscribe/post?u=fe76ad92bdd35d5808d870eb6&amp;id=d48439dd50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
                 <div class="form-group">
-                    <label class="sr-only" for="exampleInputEmail3">Email Address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email Address">
+                    <label class="sr-only" for="mce-EMAIL">Email address</label>
+                    <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="Email address" required>
                 </div>
                 <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn"><i class="fa fa-envelope"></i></button>
             </form>
