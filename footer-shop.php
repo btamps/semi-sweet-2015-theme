00e0b8dccaed6d3a81876b678c55416450f9f972
William Tamplin <billytamplin@gmail.com>
2015-11-23 22:24:36 -0800

    Moved wp_footer right before the closing body tag

--- footer-shop.php
+++ footer-shop.php
@@ -39,8 +39,8 @@
       </div>
     </div>
 
-  <?php wp_footer(); ?>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <?php wp_footer(); ?>
   </body>
 </html>
--- footer.php
+++ footer.php
@@ -38,10 +38,10 @@
         <p class="col-sm-6 made-by">Made with <span class="heart pulse2">♥</span> by BTAMPS</p>
       </div>
     </div>
-    <?php wp_footer(); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <?php wp_footer(); ?>
 
   </body>
 </html>
