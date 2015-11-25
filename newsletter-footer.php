57568a209c60a7e5a81471341adaa4cdb13cac63
William Tamplin <billytamplin@gmail.com>
2015-11-24 18:42:08 -0800

    Fixed newsletter input layout for mobile

--- newsletter-footer.php
+++ newsletter-footer.php
@@ -1,8 +1,8 @@
 <div class="container-fluid newsletter-box">
     <div class="row">
         <section class="col-sm-8 col-md-offset-2 newsletter-text">
-            <h2>Stay in the Mix Newsletter</h2>
-            <p>Receive blog and store updates delivered straight to your inbox.</p>          
+            <h2>Stay in the Mix</h2>
+            <p>Sign up for the newsletter to receive blog and store updates!</p>          
         </section>
         <section class="col-sm-6 col-sm-offset-3">
             <form action="//semisweetdesigns.us12.list-manage.com/subscribe/post?u=fe76ad92bdd35d5808d870eb6&amp;id=d48439dd50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
@@ -13,7 +13,7 @@
                 <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">Subscribe</button>
             </form>
 
-            <p class="no-joke">It's free. No spam. Unsubscribe whenever you want.</p>   
+            <p class="no-joke">It's delivered straight to your inbox. Free. No spam. Unsubscribe whenever you want.</p>   
         </section>
     </div>
 </div>
