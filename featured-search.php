d6170848e59b4855202127316bdde718fb058d02
William Tamplin <billytamplin@gmail.com>
2015-11-29 16:19:43 -0800

    Added the search bar to the blog home

---
+++ featured-search.php
@@ -0,0 +1,35 @@
+<div class="row">
+    <div class="wrapper-bg sidebar-post-box">
+        <div class="search-box">
+            <p class="archive-message"><a href="archive">Visit Archive</a></p>
+            <form role="search" method="get" id="searchform" class="searchform form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <div>
+                    <label class="sr-only" for="search">Search Blog</label>
+                    <input type="text" class="form-control" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search Blog" />
+                    <button type="submit" id="searchsubmit" class="btn"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+        <h3>Featured Posts</h3>
+        <?php
+            $args = array(
+              'post_type' => 'post',
+              'category_name' => 'featured'
+            );
+            $the_query = new WP_Query( $args );
+        ?>
+        <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+        <article class="sidebar-post">
+            <a href="<?php the_permalink(); ?>" class="thumb">
+                <?php the_post_thumbnail(); ?>
+                <div class="sidebar-post-body">
+                    <h4 class="sidebar-post-heading"><?php the_title(); ?></h4>
+                </div>
+            </a>
+        </article>
+
+    <?php endwhile; endif; ?>
+
+    </div> <!-- sidebar-post-box end -->
+</div> <!-- row end -->
--- sidebar-home.php
+++ sidebar-home.php
@@ -2,7 +2,7 @@
 <div class="col-sm-4 sidebar">
   <?php get_template_part( 'newsletter', 'side' ); ?>
   <?php get_template_part( 'google', 'ad' ); ?>
-  <?php get_template_part( 'featured', 'posts' ); ?>
+  <?php get_template_part( 'featured', 'search' ); ?>
   <?php get_template_part( 'google', 'two' ); ?>
   <?php get_template_part( 'getting', 'started' ); ?>
   <?php get_template_part( 'google', 'three' ); ?>
