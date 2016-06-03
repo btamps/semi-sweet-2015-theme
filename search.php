143e41a696cd70935a54d6e88521517ea50c835d
Billy Tamplin <billytamplin@gmail.com>
2016-06-03 13:38:17 -0700

    fixed search conditional

--- search.php
+++ search.php
@@ -7,10 +7,10 @@ get_header(); ?>
 
 <div class="container-fluid content-box">
   <div class="row">
-    <?php if ( is_search() ) {
-      //put your search results markup here (you can copy some code from archive-product.php file and also from content-product.php to create a standard markup
-        get_template_part( 'content', 'main-products' );
-
+    <?php
+      $classes = get_body_class();
+      if (in_array('archive',$classes)) {
+          get_template_part( 'content', 'main-products' );
       } else {
         get_template_part( 'content', 'results-blog' );
         get_sidebar( 'archive' );
