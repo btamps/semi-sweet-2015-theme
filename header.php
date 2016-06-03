1fd00ed390fb800713d5d676fdec492fff5ec248
Billy Tamplin <billytamplin@gmail.com>
2016-06-02 21:32:16 -0700

    added new google fonts

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
     </script>
 
     <!-- Google Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif|Lato:900,400,700' rel='stylesheet' type='text/css'>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
