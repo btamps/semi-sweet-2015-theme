51cc1d816f722d2cbfbb2c3821b9626511f1e33d
Billy Tamplin <billytamplin@gmail.com>
2016-06-06 21:03:52 -0700

    consolidated all new woocommerce related js

---
+++ js/wootweaks.js
@@ -0,0 +1,14 @@
+// Adds thumbnail hover to main product img
+jQuery(document).ready(function($) {
+  // Adds a class to .price that contains "Starting at:"
+  $( "p.price:contains('Starting at:')" ).addClass( 'starting-price' );
+
+  // Get value from select box
+  var sizeValue = $( ".select-wrap select option:selected" ).text();
+  // Place value next to text in .amount
+  console.log(sizeValue);
+  $( ".woocommerce-variation-price .amount" ).append( "<span>" + sizeValue + "</span>" );
+
+  // Removes extra br given to buttons
+  $(".button br").remove();
+});
