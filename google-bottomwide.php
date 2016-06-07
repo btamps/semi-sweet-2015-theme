43d63d376a3c06d95d0aceae2b619fc288d7be67
Billy Tamplin <billytamplin@gmail.com>
2016-06-06 21:02:51 -0700

    added bottom ad

---
+++ google-bottomwide.php
@@ -0,0 +1,15 @@
+<!-- Bottom Wide Ad -->
+<div class="ad-bottom-wide">
+  <div class="ad-320x100">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Homepage Bottom Ad -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:320px;height:100px"
+         data-ad-client="ca-pub-3606452520556010"
+         data-ad-slot="5864810333"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+  </div>
+</div>
