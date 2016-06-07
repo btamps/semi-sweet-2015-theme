9fd999e1872d3ca7d4d20b5f5f0cca3b64676828
Billy Tamplin <billytamplin@gmail.com>
2016-06-06 21:09:51 -0700

    changed the cart conditional

--- nav-bar.php
+++ nav-bar.php
@@ -19,9 +19,9 @@
         <li><a href="//www.facebook.com/semisweetdesigns"><i class="fa fa-facebook"></i></a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <?php 
-          if ( WC()->cart->get_cart_contents_count() > 0 ) { ?>
-            <li><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '<i class="fa fa-shopping-cart"></i> (%d)', '<i class="fa fa-shopping-cart"></i> (%d)', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ); ?></a></li>  
+        <?php
+          if ( WC()->cart->get_cart_contents_count() != 0 ) { ?>
+            <li><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf (_n( '<i class="fa fa-shopping-cart"></i> (%d)', '<i class="fa fa-shopping-cart"></i> (%d)', WC()->cart->cart_contents_count ), WC()->cart->cart_contents_count ); ?></a></li>
         <?php }
         ?>
         <?php
@@ -33,7 +33,7 @@
           echo strip_tags(wp_nav_menu( $args ), '<li><a>');
 
         ?>
-      </ul>          
+      </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
