eae5bb591c823779c4015efe70c0d9a54434842f
Billy Tamplin <billytamplin@gmail.com>
2017-10-09 21:52:39 -0700

    new styles for the newsletter block in sidebar

--- newsletter-side.php
+++ newsletter-side.php
@@ -1,15 +1,15 @@
 <div class="row">
-    <div class="wrapper-bg newsletter">
-        <div class="inner">
-            <h3>Subscribe to the Newsletter</h3>  
-            <h4>Get new blog and store updates delivered to your inbox.</h4>
-            <form action="//semisweetdesigns.us12.list-manage.com/subscribe/post?u=fe76ad92bdd35d5808d870eb6&amp;id=d48439dd50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
-                <div class="form-group">
-                    <label class="sr-only" for="mce-EMAIL">Email address</label>
-                    <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="Email address" required>
-                </div>
-                <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn"><i class="fa fa-envelope"></i></button>
-            </form>
-        </div>
+    <div class="newsletter">
+      <img src="<?php bloginfo('template_directory'); ?>/ui/stay-in-the-mix-objects.png" alt="Stay in the Mix Newsletter">
+      <h2>Stay in the Mix</h2>
+      <h3>Newsletter</h3>
+      <p>Get new blog and store updates delivered to your inbox.</p>
+      <form action="//semisweetdesigns.us12.list-manage.com/subscribe/post?u=fe76ad92bdd35d5808d870eb6&amp;id=d48439dd50" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
+          <div class="form-group">
+              <label class="sr-only" for="mce-EMAIL">Your email address</label>
+              <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="Your email address" required>
+          </div>
+          <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn"><i class="fa fa-envelope-o"></i></button>
+      </form>
     </div>
 </div>
